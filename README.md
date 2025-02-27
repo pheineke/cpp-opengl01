34f3c2ee1692fc5d87f21d1180f86d761fc6567b
p_heineke <heineke.pe@gmail.com>
2025-02-27 01:13:40 +0100

    > Finished "Getting Started > Hello Window Chapter" 27.02.2025

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning OpenGL with:
+[https://learnopengl.com/](learnopengl.com)
+> Finished "Getting Started > Hello Window Chapter" 27.02.2025 
--- cpp-opengl01/cpp-opengl01.cpp
+++ cpp-opengl01/cpp-opengl01.cpp
@@ -2,8 +2,16 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+
+
+//callback function for window resize/changes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+//Input Event Handler
+void processInput(GLFWwindow* window);
 
 int main()
 {
@@ -13,15 +21,71 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
+	//Get current function set of OS were compiling for
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	//Render loop
+	//If instructed to close then stop
+	int i = 0;
+
+	while (!glfwWindowShouldClose(window)) {
+
+		//Input
+		processInput(window);
+
+		//Set new color
+		if (i % 2 == 0) {
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+		}
+		else {
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+		}
+
+		
+
+		i += 1;
+
+
+		//Swap Buffers
+		glfwSwapBuffers(window);
+		//Check for events triffered (keyboard input, mouse input etc.)
+		//new ones can be registered with callback functions like window resize
+		glfwPollEvents();
+	}
+	
+	//Clean ressources allocated to GLFW
+	glfwTerminate();
 	return 0;
 
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
 
 
-
-
+//Input event function
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+}
 
 
 
